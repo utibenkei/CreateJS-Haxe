7a5eee5e6cc8ce571e8a7abc60d922a980bef70b
utibenkei <utibenkei2@gmail.com>
2014-12-11 20:51:21 +0900

    Fix fix BitmapText

--- createjs/easeljs/BitmapText.hx
+++ createjs/easeljs/BitmapText.hx
@@ -5,8 +5,8 @@ extern class BitmapText extends DisplayObject {
 
 	public function new(?text:String="", ?spriteSheet:SpriteSheet=null):Void;
 	public var letterSpacing:Float;
-	public var lineHeight:Number;
-	public var spaceWidth:Number;
+	public var lineHeight:Float;
+	public var spaceWidth:Float;
 	public var spriteSheet:SpriteSheet;
 	public var text:String;
 }
